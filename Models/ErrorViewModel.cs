63d2a8c30eec6480cd91c5fb099c1e5ba99b8ef2
andrijaracic <aracic4022it@raf.rs>
2025-11-01 16:50:17 +0100

    Postavljen projekat i povezan sa bazom

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoopLeague.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
